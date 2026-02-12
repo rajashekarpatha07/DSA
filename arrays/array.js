36e93f3c3a6ae6e57332b48ead9963fd92f71fdc
rajashekarpatha07 <rajashekarpatha07@gmail.com>
2026-02-12 10:12:51 +0530

    add: fuction to delete element at index

--- arrays/array.js
+++ arrays/array.js
@@ -31,15 +31,42 @@ class Array {
     }
     this.data[0] = element;
 
-
     this.length++;
 
     return this.data;
+  }
+
+  // Array = ["A", "B", "C", "D"]
+  delete(index) {
+    //check weather the index is out of bound or not
+    if (index < 0 || index >= this.length) return "index out of bound";
+
+    // store the element that to be deleted in an variable
+    const elementtobeDeleted = this.data[index]; // Element "B" is stored in this variable
+
+    for (let i = index; i < this.length - 1; i++) {
+      // at the first itration this.data[i] = B, this.data[i+1] = C it became ["A", "C", "C", "D"],
+      // in the next itration it becomes ["A","C","D", "D"]
+      // now we delete the last element with delete this.data[this.length - 1];
+      // now we decrese the length of array by 1
+
+      this.data[i] = this.data[i + 1];
+    }
+
+    delete this.data[this.length - 1];
+    this.length--;
+
+    return elementtobeDeleted
   }
 }
 
 const myArray = new Array();
 
-console.log(myArray.insert(20));
+myArray.insert(19);
+myArray.insert(12);
+myArray.insert(18);
+myArray.insertAtEnd(8);
+myArray.insert(70);
+myArray.insertAtFirst(100);
 
-console.log(myArray.get(0));
+console.log(myArray);
